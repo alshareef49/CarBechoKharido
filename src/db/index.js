14424ca036d7a05a8dc1ec7054556bbe0fc0abd9
alshareef49 <alshareef4039@gmail.com>
2024-03-07 17:04:38 +0530

    added index for respective collection

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,13 @@ const connectDB = async ()=> {
     try {
         const client = await MongoClient.connect(`${process.env.MONGODB_URI}`);
         db = client.db(`${DB_NAME}`);
+        await db.collection('users').createIndex({ user_email: 1 }, { unique: true });
+        await db.collection('sold_vehicles').createIndex({ vehicle_id: 1 }, { unique: true });
+        await db.collection('dealerships').createIndex({ dealership_email: 1 }, { unique: true });
+        await db.collection('cars').createIndex({ car_id: 1 }, { unique: true });
+        await db.collection('deals').createIndex({ deal_id: 1 }, { unique: true });
+        await db.collection('admins').createIndex({ admin_id: 1 }, { unique: true });
+        
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
